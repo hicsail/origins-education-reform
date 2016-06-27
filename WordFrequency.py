5308b2cd2b8015209306d377a902f477a1720dbf
bengetch <bengetch@gmail.com>
2016-06-26 20:05:00 -0400

    Update WordFrequency.py
    Rewrote the script to incorporate I/O, simplified the methods, etc.
    Currently working on the plotting functions and normalizing the data using TF-IDF

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,87 +1,117 @@
+
 import json
 import os
 import nltk
-import re
-import matplotlib.pyplot as plt
+import argparse
 
-decades = [1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870,
-           1880, 1890, 1900, 1910, 1920]
+def buildKeyList(keywords):
+    keyList = keywords.split()
+    return keyList
 
+def buildDecadesList(min, max):
+    decadesMin = int(min)
+    decadesMax = int(max)
+    numElements = ((decadesMax - decadesMin) / 10)
+    decades = [None] * int(numElements)
+    i = 0
+    for num in range(decadesMin, decadesMax, 10):
+        decades[i] = num
+        i += 1
+    return sorted(decades)
 
-keywords = ["folk", re.compile(r"\bgive\b|\bgiver\b"), "pligt",
-            re.compile(r'\brettighed\b|\brettigheder\b'), "domstol",
-            "marked"]
-results = {}
-decadeTally = {}
-
-#Can just build this dynamically (with the try block)
-for decade in decades:
-    for keyword in keywords:
-        try:
-            results[decade][keyword] = 0
-        except KeyError:
-            results[decade] = {keyword: 0}
-
-for decade in decades:
-    try:
-        decadeTally[decade] = 0
-    except KeyError:
-        pass
-###
-#Specify input directory here
-###
-directory = ""
-
-for subdir, dirs, files in os.walk(directory):
-    for jsondoc in files:
-        with open(directory + "/" + jsondoc, 'r') as input:
-            jsondata = json.load(input)
-            text = jsondata["8.Full Text"]
-            year = jsondata["4.Year Published"]
-            decade = int(year) - int(year)%10
+def buildDict(decades, keywords):
+    results = {}
+    for decade in decades:
+        for keyword in keywords:
             try:
-                decadeTally[decade] += 1
+                results[decade][keyword] = 0
             except KeyError:
-                pass
-            textList = text.split()
-            stripped_text = [word.strip(",._-:;\"\'()0123456789!?") for word in textList]
-            fdist = nltk.FreqDist(stripped_text)
-            for word in fdist.keys():
-                for keyword in keywords:
+                results[decade] = {keyword: 0}
+    return results
+
+def buildDecadeTally(decades):
+    decadeTally = {}
+    for decade in decades:
+        try:
+            decadeTally[decade] = 0
+        except KeyError:
+            pass
+    return decadeTally
+
+def populateDecadeTally(directory, decadeTally):
+    for subdir, dirs, files in os.walk(directory):
+        for jsondoc in files:
+            with open(directory + "/" + jsondoc, 'r') as input:
+                jsondata = json.load(input)
+                text = jsondata["8.Full Text"]
+                year = jsondata["4.Year Published"]
+                decade = int(year) - int(year)%10
+                if text == "":
+                    pass
+                else:
                     try:
-                        if keyword.match(word.lower()):
-                            try:
-                                results[decade][keyword] += fdist[word]
-                            except KeyError:
-                                # Word not found in fdist
-                                pass
-                    except AttributeError:
-                        if word.lower() == keyword:
-                            try:
-                                results[decade][keyword] += fdist[word]
-                            except KeyError:
-                                # Word not found in fdist
-                                pass
+                        decadeTally[decade] += 1
+                    except KeyError:
+                        pass
+    return decadeTally
 
-for keyword in keywords:
-    i = 0
-    a = [0]*(len(decades))
-    while i < len(decades):
-        a[i] += round(results[decades[i]][keyword] / decadeTally[decades[i]], 2)
-        i += 1
-    print(keyword)
-    print(a)
+def buildFreqDist(directory, keywords, results):
+    for subdir, dirs, files in os.walk(directory):
+        for jsondoc in files:
+            with open(directory + "/" + jsondoc, 'r') as input:
+                jsondata = json.load(input)
+                text = jsondata["8.Full Text"]
+                year = jsondata["4.Year Published"]
+                decade = int(year) - int(year)%10
+                textList = text.split()
+                stripped_text = [word.strip(",._-:;\"\'()0123456789!?") for word in textList]
+                cleaned_text = [word.lower() for word in stripped_text]
+                if text == "":
+                    pass
+                else:
+                    fdist = nltk.FreqDist(cleaned_text)
+                    for keyword in keywords:
+                        words = keyword.split("/")
+                        for w in words:
+                            try:
+                                num = fdist[w]
+                                results[decade][keyword] += num
+                            except KeyError:
+                                pass
+    return results
 
-for keyword in keywords:
-    i = 0
-    a = [0]*(len(decades))
-    while i < len(decades):
-        a[i] += results[decades[i]][keyword] / decadeTally[decades[i]]
-        i += 1
-    plt.plot(decades, a, label=keyword)
-plt.legend(bbox_to_anchor=(0, 1.02, 1., .102), loc=3, ncol=3,
-           mode="expand", borderaxespad=0.)
-plt.xlabel("Decade")
-plt.ylabel("Word Frequency")
-plt.axis([1800, 1920, 0, 85])
-plt.show()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", metavar='in-directory', action="store", help="input directory argument")
+    parser.add_argument("-o", help="output file argument", action="store")
+    parser.add_argument("-k", help="list of keywords argument, surround list with quotes", action="store")
+    parser.add_argument("-d", help="min/max for decades list, surround with quotes", action="store")
+
+    try:
+        args = parser.parse_args()
+    except IOError:
+        pass
+
+    directory = args.i
+    keywords = buildKeyList(args.k)
+    min_max = args.d.split()
+    decades = buildDecadesList(min_max[0], min_max[1])
+
+    results = buildDict(decades, keywords)
+    results = buildFreqDist(directory, keywords, results)
+
+    decadeTally = buildDecadeTally(decades)
+    populateDecadeTally(directory, decadeTally)
+
+    out = open(args.o + ".txt", 'w')
+
+    for decade in decades:
+        out.write("Decade: " + str(decade) + "\n")
+        out.write("Number of books for this decade: " + str(decadeTally[decade]) + "\n")
+        for keyword in keywords:
+            out.write(keyword + ":")
+            out.write(str(results[decade][keyword]) + "\n")
+        out.write("\n")
+
+if __name__ == '__main__':
+    main()
