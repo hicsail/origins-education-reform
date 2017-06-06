8ac84610fd9234fd49c1a1e91827258dbabfbb54
ch3njust1n <levelicarus@gmail.com>
2017-06-06 18:11:23 -0400

    Fixed parsing and resume functions and updated README

---
+++ parliament/README.md
@@ -0,0 +1,115 @@
+### Author: Justin Chen
+#### 5.20.17
+
+### Requirements
+- Python 3
+- BeautifulSoup 4
+- urllib
+- multiprocessing
+- datetime
+
+### Basic usage
+```
+$ python parliament.py --save my_data
+```
+This will scrape the Hansard Parliament website for all sittings starting from 1802-07-02 (July 2nd, 1802) to 2006-0424 (April 24th, 2006) and save all the data in the directory my_data. If the directory does not exist, the script will create it.
+
+#### Search by date range
+```
+$ python parliament.py --start_year 1803 --start_month 11 --start_day 22 --end_year 1803 --end_month 12 --end_day 20
+```
+If you just want to search for all sittings in a specific year without specifying a particular month and day, just set `--end_month 12` and `end_day 31`.
+
+#### Resume search
+If you need to resume the search, use the `--resume` flag, which will load the available data from the `save` directory and determine which files are missing and where to resume the search. This flag can also be used in conjunction with the date flags to resume and stop at a specified date. This assumes all the files from the previous run exist in `save`.
+```
+$ python parliament.py --resume
+```
+
+#### Parallelize
+The Hansard website seems to get easily overwheelmed, so specify a narrow date range when using this flag to avoid having to resume.
+```
+$ python parliament.py --parallel --start_year 1803 --start_month 11 --start_day 22 --end_year 1803 --end_month 12 --end_day 20
+```
+
+#### Example sitting output
+All sittings are saved in JSON files and are named using the convention year-month-day-sitting_title.js. For example:
+```
+1803-11-23-boston_election_fetitiom.js
+
+{'author': 'UK Parliament',
+ 'content': {'header': ['BOSTON ELECTION FETITIOM.\\u2014',
+                        'HC Deb 23 November 1803 vol 1 cc32-3'],
+             'text': ['Mr. Ellison\n\n            adverted to the petition of John Ogle, Esq. complaing of an undue election and return for the borough of Boston, in the county of Lincoln, against Mr. Fydell, the present sitting member. He desired that the entry on the journals respecting this petition, might be read; which was accordingly done. He then desired that an entry on the journals in the year 1744, relative to the case of a petitioner who had been allowed seven days time to give in his qualification, on account of his being beyond seas at the time he was served with notice to do so, which he said was precisely the case of the present sitting member, Mr. Fydell; which entry being read, he moved, "that Mr. Fydell be allowed seven days from this present day, in order to prepare and lay before the house, a particular account of his qualification to be admitted as a member."\n          \n\n\n33',
+                      "The Speaker\n\n            entered into a foil explanation of the case. He reminded the house of the jealousy with which they were always actuated in matters of this nature. Yet he was well aware that laudable as that jealousy was, it would never stand in the way of justice and equity when they could be fairly attended without prejudice to either party. It was determined, by an order of the house, that when the qualification of candidates was called in question, the particulars of that qualification should be produced by him within fifteen days after the complaint of his wanting it, was made against him. The usual notice, it appears, had been sent to Mr. Fydell, in the present case, but not complied with; and the reason adduced for his non-compliance was his indispensible absence beyond sea. A precedent, similar to the present case, occured in the year 1744. It respected also a naval officer who was necessarily absent on the service of his country; and the house then judged it fit to extend the time. Should the house be now inclined to grant the same indulgence, they would agree to the motion made by the honourable gent.\\u2014Mr. Ellison's motion was then put and agreed to."]},
+ 'date': '1803-11-23',
+ 'url': 'http://hansard.millbanksystems.com/commons/1803/nov/23/boston-election-fetitiom'}
+```
+***Note: When loading this data, should parse it for newlines, tabs, and white spaces as appropriate for your use. There are some odd sittings such as [1803-12-20-commons-preamble](http://hansard.millbanksystems.com/commons/1803/dec/20/preamble)
+
+### Directory structure
+```
+- parliament
+	- parliament.py
+	- save
+		- api
+		- chkpt
+			- sittings
+			- timeline_urls.p
+		- sittings
+
+```
+
+#### save/api directory
+This directory contains the JSON from Hansard's API. Files were named by date e.g. 1802-07-02.js (year-month-day). This contains the meta data for the sitting on the given date.
+```
+1802-07-02.js
+
+[{'house_of_commons_sitting': {'chairman': null,
+                               'data_file_id': 52,
+                               'date_text': 'Tuesday, July 2.',
+                               'end_column': '720',
+                               'id': 48,
+                               'start_column': '718',
+                               'title': 'HOUSE OF COMMONS.',
+                               'top_level_sections': [{'section': {'date': '1802-07-02',
+                                                                   'end_column': '719',
+                                                                   'id': 196,
+                                                                   'parent_section_id': 195,
+                                                                   'sitting_id': 48,
+                                                                   'slug': 'minutes',
+                                                                   'start_column': '718',
+                                                                   'title': 'Minutes.'}},
+                                                      {'section': {'date': '1802-07-02',
+                                                                   'end_column': '720',
+                                                                   'id': 197,
+                                                                   'parent_section_id': 195,
+                                                                   'sitting_id': 48,
+                                                                   'slug': 'trotters-indemnity-bill',
+                                                                   'start_column': '719',
+                                                                   'title': "TROTTER'S INDEMNITY BILL."}},
+                                                      {'section': {'date': '1802-07-02',
+                                                                   'end_column': '720',
+                                                                   'id': 198,
+                                                                   'parent_section_id': 195,
+                                                                   'sitting_id': 48,
+                                                                   'slug': 'duke-of-atholls-claim',
+                                                                   'start_column': '720',
+                                                                   'title': "DUKE OF ATHOLL'S CLAIM."}}],
+                               'volume_id': 4,
+                               'year': 1802}}]
+```
+
+#### save/chkpt/sittings directory
+This directory contains the checkpointing information for resuming the script. Each file is named by date e.g. 1802-07-02.js (year-month-day). Each file only contains the titles of all sittings on the given date and the partial URL to that sitting. For example, the DUKE OF ATHOLL'S CLAIM sitting is at http://hansard.millbanksystems.com/commons/1802/jul/02/duke-of-atholls-claim. These files must exist in this location to resume the script from where it left off.
+```
+1802-07-02.js
+
+{"DUKE OF ATHOLL'S CLAIM.": '/commons/1802/jul/02/duke-of-atholls-claim',
+ 'Minutes.': '/commons/1802/jul/02/minutes',
+ "TROTTER'S INDEMNITY BILL.": '/commons/1802/jul/02/trotters-indemnity-bill'}
+
+```
+
+#### save/chkpt/timeline_url.p
+`timeline_url.p` is a pickle file containg all the brute force generated dates for searching a range of dates. This file must exist in this location to resume the script from where it left off.
--- parliament/parliament.py
+++ parliament/parliament.py
@@ -1,30 +1,34 @@
 '''
-	Justin Chen
-	5.20.17
+    Justin Chen
+    5.20.17
     main.py
 
     Script for scraping the Commons and Lords Hansard, the Official Report of debates in Parliament website
 
     Note: Must be run with Python3 to avoid character encoding issues.
 
-	Boston University
-	Hariri Institute for Computing and Computational Science & Engineering
+    Boston University
+    Hariri Institute for Computing and Computational Science & Engineering
 '''
+
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.error import URLError
-from multiprocessing import Pool
+from urllib.error import HTTPError, URLError
+from multiprocessing import Pool, Process
 import argparse
 import json
 import os
 import time
 import random
 import codecs
+import pickle
+import re
 
 reader = codecs.getreader("utf-8")
+
+base_url = 'http://hansard.millbanksystems.com'
 
 num2month = {1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'may', 6: 'jun', 7: 'jul', 8: 'aug', 9: 'sep', 10: 'oct',
              11: 'nov', 12: 'dec'}
@@ -51,77 +55,153 @@ def daterange(start_date, end_date):
         curr += timedelta(days=1)
 
 
-def get_sittings(save_dir, base_url, start_date, end_date, parallel):
-    url_list = []
-    for date_args in daterange(start_date, end_date):
-        for site_path in sitemap:
-            date_url, doc_date = format_url(base_url, site_path, date_str=date_args)
-            if parallel:
-                url_list.append((save_dir, base_url, date_url, doc_date))
-            else:
-                scrape_thread(save_dir, base_url, date_url, doc_date)
+def get_sittings(save_dir, start_date, end_date, parallel):
+    timeline = []
+    chkpt_dir = os.path.join(save_dir, 'chkpt/timeline_urls.p')
+
+    if not os.path.exists(chkpt_dir):
+        # brute force generate all possible URLs in the date range. Some may not exists.
+        for date_args in daterange(start_date, end_date):
+            for site_path in sitemap:
+                date_url, doc_date = format_url(site_path, date_str=date_args)
+                timeline.append((save_dir, date_url, doc_date))
+
+        save(chkpt_dir, timeline)
+    else:
+        timeline = find_range(start_date, end_date, load_pickle(chkpt_dir))
 
     if parallel:
         pool = Pool()
-        pool.starmap(scrape_thread, url_list)
+        pool.starmap(scrape_thread, timeline)
         pool.close()
         pool.join()
+    else:
+        for url in timeline:
+            scrape_thread(url[0], url[1], url[2])
 
 
-def scrape_thread(save_dir, base_url, date_url, doc_date):
-    # Random delay to space out requests to mitigate overwheeling the server
-    time.sleep(random.uniform(0, 10))
+def json2str_date(s):
+    return '-'.join([str(s['year']), '%02d' % s['month'], '%02d' % s['day']])
 
+
+def find_range(start, end, timeline):
+    if type(start) != type(end):
+        raise Exception(
+            'find_range(start, end, timeline): Both inputs start and end must be of the same type (dict, str).')
+
+    if not isinstance(start, str) and not isinstance(start, dict):
+        raise Exception('find_range(): Unsupported type. ', str(type(start)), ' Must be type str or dict.')
+
+    if isinstance(start, dict):
+        start = json2str_date(start)
+        end = json2str_date(end)
+
+    start = ''.join([start, '.js'])
+    end = ''.join([end, '.js'])
+
+    if start == '1802-07-02.js' and end == '2006-04-24.js':
+        return timeline
+
+    s = e = 0
+
+    for i, t in enumerate(timeline):
+        # find the start date
+        if t[3] == start:
+            s = i
+
+        # find the end date
+        if t[3] == end:
+            e = i
+            break
+
+    return timeline[s:e]
+
+
+def scrape_thread(save_dir, date_url, doc_date):
     # Need to correctly pair the URLs and titles from the titles in the API
     bs_page = scrape(date_url[:-3])
+
+    if bs_page == None:
+        return
 
     # Get a-tag text and href
     try:
         atags = [(tag.string.strip(), tag['href']) for tag in bs_page.findAll('a', href=True)]
     except AttributeError:
         # Occurs when the page does not exist. Page may not exist because URLs were generated brute force.
+        # so just skip
         return
 
     page_text = scrape_text(bs_page)
+
     # Items listed on this page are either links to sittings or titles to pages with sittings
     # Items directly to sittings are listed as e.g. "Preamble 8 words", so need to separate these 2 types of items
+    # Potential problem is "words" appears in the section title and is not a sitting title
     line_indices = [page_text.index(c) - 1 for c in page_text if 'words' in c]
     titles = [page_text[t].strip() for t in line_indices]
 
     sittings = [tag for tag in atags for t in titles if t in tag]
 
+    if date_url.split('/')[3] == 'sittings':
+        # Save API JSON
+        api = scrape(date_url)
+        save(os.path.join(save_dir, 'api', ''.join([doc_date[:-3], '.js'])), api)
+
+        # Save all valid sitting titles on this date
+        sit_dict = {}
+        for x, y in sittings:
+            sit_dict.setdefault(remove_unicode(x), y)
+        title = doc_date[:-3]
+
+        save(os.path.join(save_dir, 'chkpt', 'sittings', ''.join([title, '.js'])), sit_dict)
+
     for sit in sittings:
-        sitting_url = ''.join([base_url, sit[1]])
-        sitting_soup = scrape(sitting_url)
-        visible_text = ''.join(sitting_soup.findAll(text=True))
+        sitting_url, content = scrape_sitting(sit)
+        save_doc(save_dir, doc_date, sitting_url, content)
 
-        # Find second occurrence of title and discard all text before it
-        # Assuming that the title occurs only twice on each sitting page, once at the top
-        #   and again before the actual sitting text - basing this on the few pages I've seen so far
-        discard_from = visible_text.find(sit[0]) + len(sit[0])
-        visible_text = visible_text[discard_from:]
-        start_pos = visible_text.find(sit[0])
 
-        # Find end of sitting text and ignore misc. text from page
-        end_pos_back = visible_text.find('Back to')
-        end_pos_forw = visible_text.find('Forward to')
+# Input: (title, partial sitting URL)
+def scrape_sitting(sit):
+    sitting_url = ''.join([base_url, sit[1]])
+    # sitting_url = 'http://hansard.millbanksystems.com/lords/1803/dec/14/minutes'
+    sitting_soup = scrape(sitting_url)
+    visible_text = ''.join(sitting_soup.findAll(text=True))
 
-        if end_pos_back == -1 and end_pos_forw == -1:
-            end_pos = len(visible_text)
-        elif end_pos_back != -1:
-            end_pos = end_pos_back
-        else:
-            end_pos = end_pos_forw
+    # Find second occurrence of title and discard all text before it
+    # Assuming that the title occurs only twice on each sitting page, once at the top
+    #   and again before the actual sitting text - basing this on the few pages I've seen so far
+    discard_from = visible_text.find(sit[0]) + len(sit[0])
+    visible_text = visible_text[discard_from:]
+    start_pos = visible_text.find(sit[0])
 
-        sitting_text = visible_text[start_pos:end_pos]
-        text_split = [x.strip() for x in sitting_text.split('§')]
-        header = [x for x in text_split[0].split('\n') if len(x) > 0][:-1]
+    sitting_text = visible_text[start_pos:]
+    split_sym = '§'
+    text_split = ''.join([x for x in sitting_text.split(split_sym) if len(x.strip()) > 0]).split('\n')
+    text_split = [x.strip() for x in text_split if len(x.strip()) > 0]
 
-        # header[0] - sitting title, header[1] - data and volume, text_split[i] - i-th paragraph/speech
-        content = {'header': header, 'text': text_split[1:]}
+    # Find end of sitting text and ignore misc. text from page
 
-        sitting = {'date': doc_date[:-3], 'url': sitting_url, 'content': content, 'author': 'UK Parliament'}
-        save_doc(save_dir, sitting, sitting['date'] + '-' + header[0][:-1].lower().replace(' ', '_'))
+    end_pos_back = end_pos_forw = end_pos_note = -1
+    for i, x in enumerate(text_split):
+        if 'Back to' in x:
+            end_pos_back = i
+        if 'Forward to' in x:
+            end_pos_forw = i
+        if 'Noticed a typo?' in x:
+            end_pos_note = i
+
+    ends = [end_pos_back, end_pos_forw, end_pos_note]
+    end_pos = min([x for x in ends if x != -1])
+    text_split = text_split[:end_pos]
+
+    cols = [tag.text.strip() for tag in sitting_soup.findAll('a', href=True) if
+            'name' in tag.attrs and 'column_' in tag.attrs['name']]
+
+    delim_header = text_split.index(cols.pop(0))
+    header = text_split[:delim_header]
+    text_split = [x for x in text_split[delim_header + 1:] if x not in cols]
+
+    return sitting_url, {'header': header, 'text': text_split}
 
 
 # Handles scraping API calls and raw HTML
@@ -137,20 +217,21 @@ def scrape(url):
     except (TimeoutError, URLError) as e:
         if e.reason == 'no host given':
             return None
-        time.sleep(random.uniform(1.5, 5))
+        time.sleep(random.uniform(1.5, 10))
         return scrape(url)
 
 
+# Get all visible text on the page
 def scrape_text(bs_html):
     return list(filter(lambda x: x != '\n', bs_html.findAll(text=True)))
 
 
-def format_url(base_url, site_path, date_str=''):
+def format_url(site_path, date_str=''):
     if len(date_str) > 0:
         url_date = date_str.split('-')
-        url_date[1] = num2month[int(url_date[1])]
         url_date[2] = url_date[2] + '.js'
         doc_date = '-'.join(url_date)
+        url_date[1] = num2month[int(url_date[1])]
         key = [k for k in site_path][0]
         url_date.insert(0, key)
         url_date.insert(0, base_url)
@@ -159,22 +240,115 @@ def format_url(base_url, site_path, date_str=''):
         return ''.join([base_url, site_path])
 
 
-def save_doc(save_dir, data, filename):
-    if len(filename) > 50:
-        filename = '_'.join(filename[:50].strip().split())
-
-    with open(os.path.join(save_dir, filename + '.json'), 'w', encoding='utf-8') as doc:
-        json.dump(data, doc)
+# full path to file including the extension
+def load_pickle(filepath):
+    with open(filepath, 'rb') as readfile:
+        return pickle.load(readfile)
 
 
-def resume_date(save_dir):
-    files = os.listdir(save_dir)
-    files.sort()
-    return files[len(files) - 1].split('-')[:3]
+def save(filepath, obj):
+    if filepath.endswith('.p'):
+        with open(filepath, 'wb') as writefile:
+            pickle.dump(obj, writefile)
+    elif filepath.endswith('.js'):
+        with open(filepath, 'w') as writefile:
+            json.dump(obj, writefile)
+    else:
+        raise Exception('Cannont save. Unfamiliar file extension.')
+
+
+def save_doc(save_dir, doc_date, sitting_url, content):
+    header = content['header']
+    sitting = {'date': doc_date[:-3], 'url': sitting_url, 'content': content, 'author': 'UK Parliament'}
+    save_dir = os.path.join(save_dir, 'sittings')
+
+    # Sitting name without backslash, periods, single and double quotes
+    title = filter_title(header[0])
+    if len(title) > 50:
+        title = title[:50]
+
+    filename = '.'.join(['-'.join([sitting['date'], sitting_url.split('/')[3], title]), 'js'])
+
+    save(os.path.join(save_dir, filename), sitting)
+
+
+def remove_unicode(s):
+    return ''.join([c for c in s if ord(c) < 128])
+
+
+def filter_title(t):
+    return re.sub('[^0-9a-zA-Z_]+', '', t.replace(' ', '_')).lower()
+
+
+def recent(a, b):
+    if type(a) != type(b):
+        raise Exception('recent(a, b): Both inputs a and b must be of the same type.')
+    a_ = a
+    b_ = b
+
+    if isinstance(a, dict):
+        a = json2str_date(a)
+        b = json2str_date(b)
+
+    return a_ if datetime.strptime(a, "%Y-%m-%d") > datetime.strptime(b, "%Y-%m-%d") else b_
+
+
+def resume(save_dir, chk_sit_dir, sit_dir):
+    # Check all sitting titles in the date range it should have gotten, which will be in chk_sit_dir
+    #  against the sittings that were actually obtained in sit_dir
+    titles_dict = {}
+    sittings = {}
+    chk_sit_files = []
+    most_recent = '0001-01-01'
+
+    for i, doc_date in enumerate(os.listdir(chk_sit_dir)):
+        with open(os.path.join(chk_sit_dir, doc_date)) as datefile:
+
+            # JSON key: sitting title, value: partial URL
+            date_sitting = json.load(datefile)
+            sittings.update(date_sitting)
+
+            # title of each file is <date>.js
+            date = doc_date.split('.')[0]
+
+            most_recent = recent(date, most_recent)
+
+            # iterate through the sittings on this date
+            for t in date_sitting:
+                filtered_title = filter_title(t)
+
+                # format all sitting titles as the sitting filenames in the sitting directory so can compare
+                # to see what should have been scraped
+                chk_sit_files.append(''.join([date, '-', date_sitting[t].split('/')[1], '-', filtered_title, '.js']))
+
+                # collect the titles
+                titles_dict[filtered_title] = t
+
+    # Retrieve any missing sittings
+    sit_dir_files = os.listdir(sit_dir)
+    diff = list(set(chk_sit_files) - set(sit_dir_files))
+
+    if len(diff) > 0:
+        for sit in diff:
+            # each sit file is named using the same convention 
+            # e.g. 1803-11-23-commons-boston_election_fetitiom.js
+            sit = sit[:-3].split('-')
+            doc_date = '-'.join([sit[0], sit[1], sit[2]]) + '.js'
+            original = remove_unicode(titles_dict[sit[-1]])
+            title_url_pair = (original, sittings[original])
+            sitting_url, content = scrape_sitting(title_url_pair)
+            save_doc(save_dir, doc_date, sitting_url, content)
+
+    last = most_recent.split('-')
+    return {'year': int(last[0]), 'month': int(last[1]), 'day': int(last[2])}
+
+
+def check_date(start_date, end_date):
+    if recent(start_date, end_date) == start_date and end_date != start_date:
+        raise Exception('main(): Start date cannot be more recent than end start')
 
 
 def main():
-    base_url = 'http://hansard.millbanksystems.com'
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save/',
                         help='Specify name of save directory. If it does not exist, it will be created.')
@@ -185,24 +359,41 @@ def main():
     parser.add_argument('--end_month', type=int, default=4, help='Month to stop searching inclusive.')
     parser.add_argument('--end_day', type=int, default=24, help='Day to stop searching.')
     parser.add_argument('--resume', type=bool, default=False,
-                        help='Resume searching from last date and do not set the start and end date range.')
+                        help='Resume searching from last date. Do not set the start and end date range if using this option.')
     parser.add_argument('--parallel', type=bool, default=False, help='Parallelize scraping.')
     args = parser.parse_args()
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
-    if args.resume:
-        last = resume_date(args.save_dir)
-        last[1] = (list(num2month.keys())[list(num2month.values()).index(last[1])])
+    sit_dir = os.path.join(args.save_dir, 'sittings')
+    if not os.path.exists(sit_dir):
+        os.makedirs(sit_dir)
 
-        start_date = {'year': int(last[0]), 'month': int(last[1]), 'day': int(last[2])}
-    else:
-        start_date = {'year': args.start_year, 'month': args.start_month, 'day': args.start_day}
+    api_dir = os.path.join(args.save_dir, 'api')
+    if not os.path.exists(api_dir):
+        os.makedirs(api_dir)
 
+    chk_dir = os.path.join(args.save_dir, 'chkpt')
+    if not os.path.exists(chk_dir):
+        os.makedirs(chk_dir)
+
+    chk_sit_dir = os.path.join(args.save_dir, 'chkpt', 'sittings')
+    if not os.path.exists(chk_sit_dir):
+        os.makedirs(chk_sit_dir)
+
+    start_date = {'year': args.start_year, 'month': args.start_month, 'day': args.start_day}
     end_date = {'year': args.end_year, 'month': args.end_month, 'day': args.end_day}
 
-    get_sittings(args.save_dir, base_url, start_date, end_date, args.parallel)
+    if args.resume:
+        resume_start_date = resume(args.save_dir, chk_sit_dir, sit_dir)
+
+        # Takes the most recent start date in the odd case that the user would like to resume, but would like to start
+        # at a more recent date, so this should skip over those dates in between and start with the new start date.
+        start_date = recent(resume_start_date, start_date)
+
+    check_date(start_date, end_date)
+    get_sittings(args.save_dir, start_date, end_date, args.parallel)
 
 
 if __name__ == "__main__":
