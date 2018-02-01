293c9d7bbeb411ec633da9a539f5ecd984a7d3df
Ben <bengetch@gmail.com>
2018-02-01 11:21:18 -0500

    begin TFIDF class

--- src/nlp/tf_idf.py
+++ src/nlp/tf_idf.py
@@ -1,3 +1,12 @@
 import json, tqdm, nltk
 from src.utils import *
+from src.nlp import Corpus
+
+
+class TFIDF(Corpus):
+
+    def __init__(self, name: str, in_dir: str, text_type: str, year_list: list, keys: [list, None]=None):
+        """ Initialize TFIDF object. """
+
+        super(TFIDF, self).__init__(name, in_dir, text_type, year_list, keys)
 
