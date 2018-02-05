5edffd6da56a6c5bf06582206b49157346abc9a8
Ben <bengetch@gmail.com>
2018-02-05 16:59:30 -0500

    move corpora dict method into base class

--- src/nlp/__init__.py
+++ src/nlp/__init__.py
@@ -1,3 +1,5 @@
+from src.utils import *
+import tqdm, json
 
 
 class Corpus:
@@ -8,18 +10,24 @@ class Corpus:
     """
 
     def __init__(self, name: str, in_dir: str, text_type: str, year_list: list,
-                 keys: [list, None]=None, stop_words: [list, None] = []):
+                 keys: [list, None]=None, stop_words: [list, None] = None):
         """ Initialize Frequency object. """
 
         self.name = name
         self.in_dir = in_dir
         self.text_type = text_type
         self.year_list = year_list
-        self.stop_words = stop_words
+        if stop_words is None:
+            self.stop_words = []
+        else:
+            self.stop_words = stop_words
         if keys is not None:
             self.key_list = self.build_keys(keys)
         else:
             self.key_list = None
+
+        self.word_to_id = None
+        self.corpora = None
 
     @staticmethod
     def build_keys(keys: list):
@@ -52,3 +60,46 @@ class Corpus:
         assert(len(lengths) == 1)
 
         return lengths.pop()
+
+    def build_dictionaries_and_corpora(self):
+        """
+        Construct word_to_id that store the word -> id mappings and
+        the bag of words representations of the documents in the corpus.
+        """
+
+        if self.word_to_id is not None:
+            return
+
+        word_to_id_results = gensim_dict(self.year_list)
+        corpora_results = list_dict(self.year_list)
+
+        print("Building word to ID mappings.")
+
+        for subdir, dirs, files in os.walk(self.in_dir):
+            for jsondoc in tqdm.tqdm(files):
+                if jsondoc[0] != ".":
+
+                    with open(self.in_dir + "/" + jsondoc, 'r', encoding='utf8') as in_file:
+
+                        jsondata = json.load(in_file)
+                        year = int(jsondata["Year Published"])
+
+                        # TODO: could extend this to n-grams, but would be slow
+                        if self.year_list[0] <= year < self.year_list[-1]:
+                            text = jsondata[self.text_type]
+
+                            for i in range(len(text) - 1, -1, -1):
+
+                                # Delete empty strings and single characters
+                                if text[i] in self.stop_words or len(text[i]) < 2:
+                                    del text[i]
+
+                            target = determine_year(year, self.year_list)
+
+                            if len(text) > 0:
+                                word_to_id_results[target].add_documents([text])
+                                d2b = word_to_id_results[target].doc2bow(text)
+                                corpora_results[target].append(d2b)
+
+        self.word_to_id = word_to_id_results
+        self.corpora = corpora_results
