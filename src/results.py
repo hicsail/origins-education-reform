1fc885678f70cbc7842e2c4479e239161b8d01a3
Ben <bengetch@gmail.com>
2018-02-05 17:00:26 -0500

    add class to display tf-idf results

--- src/results.py
+++ src/results.py
@@ -28,20 +28,17 @@ class FrequencyResults:
                     "________________\n"
                     "Period: {0} - {1}\n"
                     .format(str(self.years[i]), str(self.years[i+1]))
-                    + "\n"
                 )
                 for k in self.d[self.years[i]].items():
                     if k[0] == 'TOTAL':
                         t.write(
-                            "{0} of all keywords taken together for this period: {1}"
+                            "{0} of all keywords taken together for this period: {1}\n"
                             .format(self.f_type, str(k[1]))
-                            + "\n"
                         )
                     else:
                         t.write(
-                            "{0} of \"{1}\" for this period: {2}"
+                            "{0} of \"{1}\" for this period: {2}\n"
                             .format(self.f_type, " ".join(k[0]), str(k[1]))
-                            + "\n"
                         )
 
     def display(self, keys: [None, list]=None):
@@ -55,9 +52,9 @@ class FrequencyResults:
         for i in range(len(self.years) - 1):
             print(
                 "________________\n"
-                "Period: {0} - {1}"
+                "Period: {0} - {1}\n"
                 .format(str(self.years[i]), str(self.years[i+1]))
-                + "\n")
+            )
             for k in keys:
                 if k == 'TOTAL':
                     print(
@@ -67,9 +64,8 @@ class FrequencyResults:
                     )
                 else:
                     print(
-                        "{0} of \"{1}\" for this period: {2}"
+                        "{0} of \"{1}\" for this period: {2}\n"
                         .format(self.f_type, " ".join(k), str(self.d[self.years[i]][k]))
-                        + "\n"
                     )
 
 
@@ -81,7 +77,7 @@ class TopResults:
         self.years = [y[0] for y in self.d.items()]
 
     def write(self, out_path: str):
-        """ Write contents of FrequencyResults object to file. """
+        """ Write contents of Frequency object to file. """
 
         with open(out_path + '.txt', 'w') as t:
             print("Writing results to text file.")
@@ -89,16 +85,14 @@ class TopResults:
             for i in range(len(self.years) - 1):
                 t.write(
                     "________________\n"
-                    "Period: {0} - {1}"
+                    "Period: {0} - {1}\n"
                     .format(str(self.years[i]), str(self.years[i+1]))
-                    + "\n"
                 )
                 t.write("Top words for this period: \n")
                 for k in self.d[self.years[i]]:
                     t.write(
-                        "\"{0}\": {1}%"
+                        "\"{0}\": {1}%\n"
                         .format(" ".join(k[0]), str(k[1]))
-                        + "\n"
                     )
 
     def display(self):
@@ -107,12 +101,62 @@ class TopResults:
         for i in range(len(self.years) - 1):
             print(
                 "________________\n"
-                "Period: {0} - {1}"
+                "Period: {0} - {1}\n"
                 .format(str(self.years[i]), str(self.years[i+1]))
-                + "\n")
+            )
             print("Top words for this period:")
             for k in self.d[self.years[i]]:
                 print(
                     "\"{0}\": {1}%"
                     .format(" ".join(k[0]), str(k[1]))
+                )
+
+
+class TfidfResults:
+    """ Data structure that stores documents with the highest TF-IDF score per period. """
+
+    def __init__(self, d: dict, keyword: str, name: [None, str]=None):
+
+        self.d = d
+        self.years = [y[0] for y in self.d.items()]
+        self.keyword = keyword
+        self.name = name
+
+    def write(self, out_path: str):
+        """ Write contents of Tfidf object to file. """
+
+        with open(out_path + '.txt', 'w') as t:
+            print("Writing results to text file.")
+
+            for i in range(len(self.years) - 1):
+                t.write(
+                    "________________\n"
+                    "Period: {0} - {1}\n"
+                    .format(str(self.years[i]), str(self.years[i+1]))
+                )
+                t.write(
+                    "Documents with the highest TF-IDF score for \'{0}\' in this period: \n"
+                    .format(self.keyword)
+                )
+                for k in self.d[self.years[i]]:
+                    t.write(
+                        "\"{0}\": {1}%\n"
+                        .format(k[0], str(k[1]))
+                    )
+
+    def display(self):
+        """ Display Tfidf results in console. """
+
+        for i in range(len(self.years) - 1):
+            print(
+                "________________\n"
+                "Period: {0} - {1}\n"
+                .format(str(self.years[i]), str(self.years[i+1]))
+            )
+            print("Documents with the highest TF-IDF score for \'{0}\' in this period: \n"
+                  .format(self.keyword))
+            for k in self.d[self.years[i]]:
+                print(
+                    "\"{0}\": {1}%"
+                    .format(k[0], str(k[1]))
                 )
