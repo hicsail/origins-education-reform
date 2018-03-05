a0b1467451f03e86595fa0e899113d98be95fc5d
Ben <bengetch@gmail.com>
2018-03-05 16:09:52 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name             = 'origins-education-reform',
+    version          = '0.0.0.1',
+    install_requires = ['gensim', 'nltk', 'matplotlib', 'numpy', 'scipy', 'statsmodels'],
+    license          = 'MIT',
+    url              = 'https://github.com/hicsail/origins-education-reform',
+    description      = 'NLP library for large corpora.'
+)
+
