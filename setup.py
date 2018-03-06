4c00901808e28137d81d46032ac4cd1095b4a5bd
Ben <bengetch@gmail.com>
2018-03-06 10:17:31 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name             = 'origins-education-reform',
+    name             = 'corpus_nlp',
     version          = '0.0.0.1',
     install_requires = ['gensim', 'nltk', 'matplotlib', 'numpy', 'scipy', 'statsmodels'],
     license          = 'MIT',
